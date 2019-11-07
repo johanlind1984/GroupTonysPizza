fd8813147a2077d11e9c52f9d032ec22af4b99b3
johanlind184 <johanlind1984@gmail.com>
2019-11-07 19:18:48 +0100

    null

--- src/sample/Pizza.java
+++ src/sample/Pizza.java
@@ -1,75 +1,41 @@
 
-
+ // Written by Tadevos
 public class Pizza {
 
-        private String namn;
+        private String name;
         private double price;
         private int ordernumber;
         //default constructor
         public Pizza() {
         }
 
-
-        public Pizza(String namn, double price, int ordernumber) {
-            this.namn = namn;
+        public Pizza(String name, double price, int ordernumber) {
+            this.name = name;
             this.price = price;
             this.ordernumber = ordernumber;
         }
 
-
-        /**
-         * Get the value of ordernumber
-         *
-         * @return the value of ordernumber
-         */
         public int getOrdernumber() {
             return ordernumber;
         }
 
-        /**
-         * Set the value of ordernumber
-         *
-         * @param ordernumber new value of ordernumber
-         */
         public void setOrdernumber(int ordernumber) {
             this.ordernumber = ordernumber;
         }
 
-
-        /**
-         * Get the value of price
-         *
-         * @return the value of price
-         */
         public double getPrice() {
             return price;
         }
 
-        /**
-         * Set the value of price
-         *
-         * @param price new value of price
-         */
         public void setPrice(double price) {
             this.price = price;
         }
 
-
-        /**
-         * Get the value of namn
-         *
-         * @return the value of namn
-         */
-        public String getNamn() {
-            return namn;
+        public String getName() {
+            return name;
         }
 
-        /**
-         * Set the value of namn
-         *
-         * @param namn new value of namn
-         */
-        public void setNamn(String namn) {
-            this.namn = namn;
+        public void setName(String name) {
+            this.name = name;
         }
     }
