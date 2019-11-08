076aa4d9c6d1d77c29c3134697f5937e4fafdbfd
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2019-11-08 23:26:47 +0100

    Update Pizza.java
    Added ArrayLists includedIngredients and extraIngredients, added methods addIngredientIncluded, addIngredientExtra, removeIngredient, toString and getters for ArraLists

--- src/sample/Pizza.java
+++ src/sample/Pizza.java
@@ -1,16 +1,47 @@
 package sample;
 
+import java.util.ArrayList;
+
 // Written by Tadevos
 public class Pizza {
 
     private String name;
     private double price;
     private int ordernumber;
+    private ArrayList<Ingredient> includedIngredients;
+    private ArrayList<Ingredient> extraIngredients;
 
     public Pizza(String name, double price) {
         this.name = name;
         this.price = price;
         this.ordernumber = 0;
+    }
+
+    public void addIngredientIncluded(Ingredient ingredientToAdd) {
+        includedIngredients.add(ingredientToAdd);
+    }
+    public void addIngredientExtra(Ingredient ingredientToAdd) {
+        extraIngredients.add(ingredientToAdd);
+    }
+
+    public boolean removeIngredient(Ingredient ingredientToRemove) {
+        if(extraIngredients.contains(ingredientToRemove)) {
+            extraIngredients.remove(ingredientToRemove);
+            return true;
+        } else if (includedIngredients.contains(ingredientToRemove)) {
+            extraIngredients.remove(ingredientToRemove);
+            return true;
+        }
+
+        return false;
+    }
+
+    public ArrayList<Ingredient> getIncludedIngredients() {
+        return includedIngredients;
+    }
+
+    public ArrayList<Ingredient> getExtraIngredients() {
+        return extraIngredients;
     }
 
     public int getOrdernumber() {
@@ -37,4 +68,20 @@ public class Pizza {
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+
+        String stringToReturn = name;
+        stringToReturn += "\t" + price + "kr" + "\n";
+
+        for (Ingredient ingredient : includedIngredients) {
+            stringToReturn += ingredient.getName() + ", ";
+        }
+
+        for (Ingredient ingredient : extraIngredients) {
+            stringToReturn += ingredient.getName() + ", ";
+        }
+
+        return stringToReturn;
+    }
 }
