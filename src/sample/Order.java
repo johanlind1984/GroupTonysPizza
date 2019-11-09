d5d39a33f5d5e4bd0d88163001019b15d7a0b557
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2019-11-09 21:28:24 +0100

    Multiple changes
    ChefTerminal: fixed method addListOfPizzasToQue to take a arraylist instead of a single Pizza object.
    
    CustomerTerminal, refacored OrderCount to OrderID for consistancy with Order class. changles some methodcalls to match Order class.
    
    Order: a copy of ShoppingCart that was removed,
    
    Pizza, added isAmerican field, added code to instantiate the ArrayLists in constructor, added field orderStatus. Added setter and getter for isAmerican, getter and setter for orderstatus.

---
+++ src/sample/Order.java
@@ -0,0 +1,76 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Order {
+    private ArrayList<Pizza> pizzasInCart;
+    private ArrayList<Extras> extrasInCart;
+    int orderID;
+    double totalPriceOfOrder;
+
+    public Order()
+    {
+        pizzasInCart = new ArrayList<>() ;
+        extrasInCart = new ArrayList<>();
+        totalPriceOfOrder = 0;
+        this.orderID = 0;
+    }
+
+    public ArrayList<Pizza> getPizzas() {
+        return pizzasInCart;
+    }
+
+    public ArrayList<Extras> getExtras() {
+        return extrasInCart;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public int getOrderID() { // Retunerar order
+        return orderID;
+    }
+
+    public double getTotalPriceOfOrder(){
+
+        for (Pizza pizza : pizzasInCart) {
+            totalPriceOfOrder = totalPriceOfOrder + pizza.getPrice();
+        }
+
+        for (Extras extras : extrasInCart) {
+            totalPriceOfOrder += extras.getPrice();
+        }
+        return totalPriceOfOrder;
+    }
+
+    public void setPizzasInOrder(ArrayList<Pizza> pizzas){
+        pizzasInCart = pizzas;
+    }
+
+    public void setExtrasOrder(ArrayList<Extras> extras) {
+        extrasInCart = extras;
+    }
+
+    @Override
+    public String toString() {
+        String stringToReturn = orderID + "\n\t";
+
+        for (Pizza pizza : pizzasInCart) {
+            stringToReturn += pizza.getName() + " " + pizza.getOrderStatus() + "\n\t";
+            for (Ingredient ingredient : pizza.getExtraIngredients()) {
+                stringToReturn += "+" + ingredient.getName();
+            }
+
+        }
+
+        stringToReturn += "\n\t";
+
+        for (Extras extra : extrasInCart) {
+            stringToReturn += extra.getName() + ", ";
+        }
+
+        stringToReturn += "\n\n";
+        return stringToReturn;
+    }
+}
--- src/sample/Pizza.java
+++ src/sample/Pizza.java
@@ -8,13 +8,19 @@ public class Pizza {
     private String name;
     private double price;
     private int ordernumber;
+    private boolean isAmerican;
+    private int orderStatus;
     private ArrayList<Ingredient> includedIngredients;
     private ArrayList<Ingredient> extraIngredients;
 
-    public Pizza(String name, double price) {
+    public Pizza(String name, double price, ArrayList<Ingredient> includedIngredients) {
         this.name = name;
         this.price = price;
         this.ordernumber = 0;
+        this.isAmerican = false;
+        this.orderStatus = 0;
+        this.includedIngredients = includedIngredients;
+        this.extraIngredients = new ArrayList<>();
     }
 
     public void addIngredientIncluded(Ingredient ingredientToAdd) {
@@ -66,6 +72,22 @@ public class Pizza {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isAmerican() {
+        return isAmerican;
+    }
+
+    public int getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setAmerican(boolean american) {
+        isAmerican = american;
+    }
+
+    public void setOrderStatus(int orderStatusToSet) {
+        orderStatus = orderStatusToSet;
     }
 
     @Override
